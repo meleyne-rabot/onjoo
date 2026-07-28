1f9112154b62188af7849c8025821b42113f8d2c
meleyne <meleyne@gmail.com>
2026-07-28 19:32:43 +0200

    Nouveau logo Uno, plus harmonieux avec les autres

--- supabase/migrations/022_games_logo_urls.sql
+++ supabase/migrations/022_games_logo_urls.sql
@@ -4,4 +4,4 @@
 update games set logo_url = '/games/flip7.webp' where code = 'flip7';
 update games set logo_url = '/games/qwirkle.webp' where code = 'qwirkle';
 update games set logo_url = '/games/skyjo.jpeg' where code = 'skyjo';
-update games set logo_url = '/games/uno.png' where code = 'uno';
+update games set logo_url = '/games/uno.webp' where code = 'uno';
